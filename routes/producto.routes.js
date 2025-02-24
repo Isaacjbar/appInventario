889704662e2e05a766713e26e375216d6973526a
Erick Garcia <20233tn149@utez.edu.mx>
2025-02-24 16:24:40 -0600

    finds

--- routes/producto.routes.js
+++ routes/producto.routes.js
@@ -4,6 +4,10 @@ const ProductoController = require("../controllers/producto.controller");
 const router = express.Router();
 
 router.get("/", ProductoController.getAllProductos);
-
+router.get("/:id", ProductoController.getProductoById);
+router.get("/numSerie/:numSerie", ProductoController.getProductoByNumSerie);
+router.post("/", ProductoController.createProducto);
+router.put("/:id", ProductoController.updateProducto);
+router.delete("/:id", ProductoController.deleteProducto);
 
 module.exports = router;
